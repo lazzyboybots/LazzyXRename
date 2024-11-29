47ac630dda8d03898bba51ac85ae540b4e182319
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-29 20:29:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
+from pyrogram import Client, filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+ADMIN = ""
+DATABASE_URI = ""
+DATABASE_NAME = ""
+FORCE_SUB = ""
+LOG_CHANNEL = ""
+START_PIC = ""
+
+LazzyXRename = Client(
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    admin=ADMIN, 
+    database_uri=DATABASE_URI, 
+    database_name=DATABASE_NAME, 
+    force_sub=FORCE_SUB, 
+    log_channel=LOG_CHANNEL, 
+    start_pic=START_PIC
+) 
+
+
+print("Bot Was Started ⚡") 
+
+LazzyXRename.run() 
+  
