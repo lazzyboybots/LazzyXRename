53ca847a2da5527eb36d27f95b304c4a600e7583
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-30 18:18:08 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -1,7 +1,8 @@
 import random 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallBackQuery
-
+from helper.database import db
+from config import Config, Txt
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
