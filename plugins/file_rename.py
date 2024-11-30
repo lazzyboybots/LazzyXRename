eacc3dc3bc1c0cf9d2bcc8ee2d4518f097e3069c
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-30 07:14:53 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -1,1 +1,79 @@
+    file_path = f"downloads/{user_id}{time.time()}/{file_name}"
+    file = query.message.reply_to_message
 
+    sts = await query.message.edit("Tʀyɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅɪɴɢ....")    
+    try:
+     	path = await file.download(file_name=file_path, progress=progress_for_pyrogram,progress_args=("Dᴏᴡɴʟᴏᴀᴅ Sᴛᴀʀᴛᴇᴅ....", sts, time.time()))                    
+    except Exception as e:
+     	return await sts.edit(e)
+    duration = 0
+    try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"): duration = metadata.get('duration').seconds
+    except:
+        pass
+    
+    ph_path = None
+    media = getattr(file, file.media.value)
+    db_caption = await db.get_caption(user_id)
+    db_thumb = await db.get_thumbnail(user_id)
+
+    if db_caption:
+        try:
+            caption = db_caption.format(filename=file_name, filesize=humanbytes(media.file_size), duration=convert(duration))
+        except KeyError:
+            caption = f"**{file_name}**"
+    else:
+        caption = f"**{file_name}**"
+ 
+    if (media.thumbs or db_thumb):
+        if db_thumb:
+            ph_path = await bot.download_media(db_thumb) 
+        else:
+            ph_path = await bot.download_media(media.thumbs[0].file_id)
+        Image.open(ph_path).convert("RGB").save(ph_path)
+        img = Image.open(ph_path)
+        img.resize((320, 320))
+        img.save(ph_path, "JPEG")
+
+    await sts.edit("Tʀyɪɴɢ Tᴏ Uᴩʟᴏᴀᴅɪɴɢ....")
+    type = query.data.split("_")[1]
+    try:
+        if type == "document":
+            await sts.reply_document(
+                document=file_path,
+                thumb=ph_path, 
+                caption=caption, 
+                progress=progress_for_pyrogram,
+                progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", sts, time.time())
+            )
+        elif type == "video": 
+            await sts.reply_video(
+                video=file_path,
+                caption=caption,
+                thumb=ph_path,
+                duration=duration,
+                progress=progress_for_pyrogram,
+                progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", sts, time.time())
+            )
+        elif type == "audio": 
+            await sts.reply_audio(
+                audio=file_path,
+                caption=caption,
+                thumb=ph_path,
+                duration=duration,
+                progress=progress_for_pyrogram,
+                progress_args=("Uᴩʟᴏᴅ Sᴛᴀʀᴛᴇᴅ....", sts, time.time())
+            )
+    except Exception as e:          
+        try: 
+            os.remove(file_path)
+            os.remove(ph_path)
+            return await sts.edit(f" Eʀʀᴏʀ {e}")
+        except: pass
+        
+    try: 
+        os.remove(file_path)
+        os.remove(ph_path)
+        await sts.delete()
+    except: pass
