be40cafa5cdaa161f214e9098657968bb8c49119
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-11-30 07:16:08 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -1,3 +1,82 @@
+from pyrogram import Client, filters
+from pyrogram.enums import MessageMediaType
+from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
+
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+from helper.utils import progress_for_pyrogram, convert, humanbytes
+from helper.database import db
+
+from asyncio import sleep
+from PIL import Image
+import os, time
+
+
+@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
+async def rename_handler(client, message):
+    file = getattr(message, message.media.value)
+    filename = file.file_name  
+    if file.file_size > 4000 * 1024 * 1024 * 1024 * 1024:
+        return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 4Gʙ")
+
+    try:
+        await message.reply_text(
+            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
+    	    reply_to_message_id=message.id,  
+    	    reply_markup=ForceReply(True)
+        )       
+    except FloodWait as e:
+        await sleep(e.value)
+        await message.reply_text(
+            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
+    	    reply_to_message_id=message.id,  
+    	    reply_markup=ForceReply(True)
+        )
+    except:
+        pass
+
+
+async def force_reply_filter(_, client, message):
+    if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):
+        return True 
+    else:
+        return False 
+ 
+@Client.on_message(filters.private & filters.reply & filters.create(force_reply_filter))
+async def rename_selection(client, message):
+    reply_message = message.reply_to_message
+
+    new_name = message.text
+    await message.delete() 
+    msg = await client.get_messages(message.chat.id, reply_message.id)
+    file = msg.reply_to_message
+    media = getattr(file, file.media.value)
+    if not "." in new_name:
+        if "." in media.file_name:
+            extn = media.file_name.rsplit('.', 1)[-1]
+        else:
+            extn = "mkv"
+        new_name = new_name + "." + extn
+    await reply_message.delete()
+
+    button = [[InlineKeyboardButton("📁 Dᴏᴄᴜᴍᴇɴᴛ",callback_data = "upload_document")]]
+    if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
+        button.append([InlineKeyboardButton("🎥 Vɪᴅᴇᴏ", callback_data = "upload_video")])
+    elif file.media == MessageMediaType.AUDIO:
+        button.append([InlineKeyboardButton("🎵 Aᴜᴅɪᴏ", callback_data = "upload_audio")])
+    await message.reply(
+        text=f"**Sᴇʟᴇᴄᴛ Tʜᴇ Oᴜᴛᴩᴜᴛ Fɪʟᴇ Tyᴩᴇ**\n**• Fɪʟᴇ Nᴀᴍᴇ :-** `{new_name}`",
+        reply_to_message_id=file.id,
+        reply_markup=InlineKeyboardMarkup(button)
+    )
+
+
+@Client.on_callback_query(filters.regex("upload"))
+async def rename_callback(bot, query): 
+    user_id = query.from_user.id
+    file_name = query.message.text.split(":-")[1]
     file_path = f"downloads/{user_id}{time.time()}/{file_name}"
     file = query.message.reply_to_message
 
