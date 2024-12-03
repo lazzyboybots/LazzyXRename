15ee4352e55536410390c64cec056e2ed73d3e7d
Lazzy Boy <147271056+lazzyboybots@users.noreply.github.com>
2024-12-03 20:03:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time
     START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6860249430').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1002231947017") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
     MAX_CONCURRENT_TRANSMISSIONS = int(os.environ.get("MAX_CONCURRENT_TRANSMISSIONS", "2")) # Set the maximum amount of concurrent transmissions (uploads & downloads).
